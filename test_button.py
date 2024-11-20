cc0f6c54c4f6f0059624e81d700c31f8e50282fa
serge <sergey28290@yandex.ru>
2024-11-20 13:32:20 +0500

    update

--- test_button.py
+++ test_button.py
@@ -1,32 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 import pytest
 
 
 @pytest.fixture()
 def browser():
     options = Options()
-    # options.headless = True
     options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    # поиск нужного вебдрайвера и дружба вебдрайвера и текущей версии браузера
-    # driver = webdriver.Chrome(
-    #     service=ChromeService(ChromeDriverManager().install()),
-    #     options=options
-    # )
-    service = ChromeService(
-        executable_path=
-        r'C:\Users\serge\.wdm\drivers\chromedriver\win64\131.0.6778.85\chromedriver-win32\chromedriver.exe'
-    )
-    driver = webdriver.Chrome(
-        service=service,
-        options=options
-    )
-    # driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options=options)
     driver.maximize_window()
     yield driver
     driver.quit()
