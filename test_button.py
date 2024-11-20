4f48e2fab236f43ef3542974376771aa54ee7f2c
serge <sergey28290@yandex.ru>
2024-11-20 19:27:59 +0500

    update

--- test_button.py
+++ test_button.py
@@ -2,13 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import pytest
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 
 @pytest.fixture()
 def browser():
     options = Options()
     options.add_argument('--headless')
-    driver = webdriver.Chrome(options=options)
+    service = Service(ChromeDriverManager(driver_version="114.0.5735.90").install())
+    # driver = webdriver.Chrome(executable_path=ChromeDriverManager(driver_version="114.0.5735.90").install(), options= options)
+    driver = webdriver.Chrome(service=service, options=options)
     driver.maximize_window()
     yield driver
     driver.quit()
