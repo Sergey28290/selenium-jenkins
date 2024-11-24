bffbf82a7d18033086c9b27c4fd7e75e6d55e5d2
serge <sergey28290@yandex.ru>
2024-11-24 14:33:39 +0500

    update

--- test_button.py
+++ test_button.py
@@ -21,3 +21,4 @@ def browser():
 def test_button_exist(browser):
     browser.get('https://www.qa-practice.com/elements/button/simple')
     assert browser.find_element(By.ID, 'submit-id-submit').is_displayed()
+    assert 1 == 2
